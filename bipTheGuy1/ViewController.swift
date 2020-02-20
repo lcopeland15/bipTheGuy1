cac7309f5cf998cde6872e1f767b4aeac46f8edc
Lily Copeland <lilykcopeland@gmail.com>
2020-02-19 20:55:45 -0800

    Commit One

--- bipTheGuy1/ViewController.swift
+++ bipTheGuy1/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey you just pressed the image!")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
